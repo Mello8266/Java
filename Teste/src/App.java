41207793a02e029577e99fe784243e3b97da00aa
Vinnycius Melo <vinnycius.melo@gmail.com>
2024-01-12 23:34:46 -0300

    Initial commit

---
+++ Teste/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Caneta c1 = new Caneta();
+        c1.cor = "Azul";
+        c1.ponta = 0.5f;
+        c1.tampada = false;
+    }
+}
---
+++ Teste/src/Caneta.java
@@ -0,0 +1,27 @@
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+
+    void status() {
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Uma caneta: " + this.cor);
+        System.out.println("Ponta: " + this.cor);
+        System.out.println("Carga: " + this.carga);
+        System.out.print("Está tampada? " + this.tampada);
+    }
+
+    void rabiscar() {
+
+    }
+
+    void tampar() {
+
+    }
+
+    void destampar() {
+        
+    }
+}
