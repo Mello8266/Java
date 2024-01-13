76ea0e7167fa94953951d2ca4a209c9aa4b90007
Vinnycius Melo <vinnycius.melo@gmail.com>
2024-01-13 01:33:00 -0300

    Aula 1

--- Aula 1/src/App.java
+++ Aula 1/src/App.java
@@ -1,8 +1,23 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Caneta c1 = new Caneta();
+        //! Atributos 
+        c1.modelo = "Bic"; // Chamada de um atributo
         c1.cor = "Azul";
         c1.ponta = 0.5f;
-        c1.tampada = false;
+        c1.carga = 90;
+
+        // * Metódos
+        c1.tampar();  // Chamada de método
+        c1.rabiscar();
+        c1.status();
+
+        Caneta c2 = new Caneta();
+        c2.modelo = "Fab castle";
+        c2.cor = "Preta";
+
+        c2.destampar();
+        c2.rabiscar();
+        c2.status();        
     }
 }
--- Aula 1/src/Caneta.java
+++ Aula 1/src/Caneta.java
@@ -1,10 +1,13 @@
+// ? Classe - molde
 public class Caneta {
+    //! Atributos
     String modelo;
     String cor;
     float ponta;
     int carga;
     boolean tampada;
 
+    //* Metódos
     void status() {
         System.out.println("Modelo: " + this.modelo);
         System.out.println("Uma caneta: " + this.cor);
@@ -14,14 +17,20 @@ public class Caneta {
     }
 
     void rabiscar() {
+        if (this.tampada == true){
+            System.out.println("Erro! Não pode rabiscar");
+        } 
+        else{
+            System.out.println("Rabiscando...");
+        }
 
     }
 
     void tampar() {
-
+        this.tampada = true; // this é o nome do objeto que chamou a classe
     }
 
     void destampar() {
-        
+        this.tampada = false;
     }
 }
