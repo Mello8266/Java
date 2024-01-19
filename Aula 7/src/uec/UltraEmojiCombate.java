5aab03c0a0ff49c45d339202a9dea200526f57fd
Vinnycius Melo <vinnycius.melo@gmail.com>
2024-01-19 15:15:14 -0300

    Update

--- Aula 7/src/uec/UltraEmojiCombate.java
+++ Aula 7/src/uec/UltraEmojiCombate.java
@@ -1,7 +1,6 @@
 package uec;
 public class UltraEmojiCombate {
     public static void main(String[] args) throws Exception {
-        // String no, String na, int  id, float al, String pe, int vi, int de, int em
         Lutador l[] = new Lutador[6];
         l[0] = new Lutador("Pretty Boy", "França", 31, 1.75f, 68.9f, 11, 3, 1);
         l[1] = new Lutador("Putscript", "Brasil", 29, 1.68f, 57.8f, 14, 2, 3);
